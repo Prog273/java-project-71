6183c826371f180ad373f10ce5357f9cd57718e5
Prog273 <dimitrius273@mail.ru>
2024-09-15 00:42:17 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("info.picocli:picocli:4.7.6")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Runnable {
+
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
         System.out.println("Hello World!");
+    }
+
+    @Override
+    public void run() {
+
     }
 }
