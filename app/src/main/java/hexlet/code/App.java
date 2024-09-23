89e2391daa4d614cd7b55f49e1a464decac17031
Prog273 <dimitrius273@mail.ru>
2024-09-23 14:39:12 +0300

    Update App.java, build.gradle.kts

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,16 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 
-import java.util.concurrent.Callable;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 @Command(name = "gendiff",
         mixinStandardHelpOptions = true,
@@ -22,6 +27,18 @@ public class App implements Runnable {
 
     @Option(names = {"-f", "--format"}, description = "output format [default: ${DEFAULT-VALUE}]", defaultValue = "stylish")
     private String format;
+
+    public void readAndParseFiles() throws IOException {
+        filepath1 = "/home/dim273/java-project-71/app/file1.json";
+        filepath2 = "/home/dim273/java-project-71/app/file2.json";
+
+        Path path1 = Paths.get(filepath1).toAbsolutePath().normalize();
+        Path path2 = Paths.get(filepath2).toAbsolutePath().normalize();
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map1 = mapper.readValue(new File(filepath1), Map.class);
+        Map<String, Object> map2 = mapper.readValue(new File(filepath2), Map.class);
+    }
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
