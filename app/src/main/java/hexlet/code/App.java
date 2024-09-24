3610e389fd22cec4c90670bb97592a777454e631
Prog273 <dimitrius273@mail.ru>
2024-09-24 14:39:24 +0300

    Update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,7 @@ import picocli.CommandLine.Command;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
@@ -28,17 +29,29 @@ public class App implements Runnable {
     @Option(names = {"-f", "--format"}, description = "output format [default: ${DEFAULT-VALUE}]", defaultValue = "stylish")
     private String format;
 
-    public void readAndParseFiles() throws IOException {
-        filepath1 = "/home/dim273/java-project-71/app/file1.json";
-        filepath2 = "/home/dim273/java-project-71/app/file2.json";
+    public void readAndParseFiles() throws Exception {
+            filepath1 = "/home/dim273/java-project-71/app/file1.json";
+            filepath2 = "/home/dim273/java-project-71/app/file2.json";
 
-        Path path1 = Paths.get(filepath1).toAbsolutePath().normalize();
+            Path path1 = Paths.get(filepath1).toAbsolutePath().normalize();
+        if (!Files.exists(path1)) {
+            throw new Exception("File '" + path1 + "' does not exist");
+        }
+
         Path path2 = Paths.get(filepath2).toAbsolutePath().normalize();
+        if (!Files.exists(path2)) {
+            throw new Exception("File '" + path2 + "' does not exist");
+        }
 
+        String content1 = Files.readString(path1);
+        String content2 = Files.readString(path2);
+
+        System.out.println(content1);
+        System.out.println(content2);
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> map1 = mapper.readValue(new File(filepath1), Map.class);
-        Map<String, Object> map2 = mapper.readValue(new File(filepath2), Map.class);
-    }
+            Map<String, Object> map1 = mapper.readValue(content1, Map.class);
+            Map<String, Object> map2 = mapper.readValue(content2, Map.class);
+        }
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
